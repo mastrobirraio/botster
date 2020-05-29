e193ada33032a079357ad5c40e5dc2373d276932
Pino Matranga <pino.matranga1999@gmail.com>
2020-05-29 17:11:12 +0200

    improved project scaffolding process and added optional path to arg

--- botster/utils.py
+++ botster/utils.py
@@ -3,3 +3,14 @@ from logthon import Logthon
 
 def setup_logger(module_name=__name__):
     return Logthon(module_name=module_name)
+
+
+class Settings:
+
+    BOT_KEY = Nonebotster/utils.py
+
+    def __init__(self):
+        from dotenv import load_dotenv
+        from os import getenv as os_get_env_variable
+
+        load_dotenv()
