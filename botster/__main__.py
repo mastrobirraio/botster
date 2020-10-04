fab48519e0a3b1ab2759ad2ea9ee497458a867ca
mastrobirraio <matrangagiuseppe99@gmail.com>
2020-10-04 13:23:36 +0200

    added notation to botster-cli

--- botster/__main__.py
+++ botster/__main__.py
@@ -1,3 +1,5 @@
 from botster.cli.__main__ import execute_from_commandline
 
+# Setup function create a script as entrypoint for cli operations.
+# Get more info running from your cli: botster-cli -h
 execute_from_commandline()
